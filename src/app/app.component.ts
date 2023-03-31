4e0adc343f442eb92d151a2d565196b01c4b9287
Jampool G <jampoolgarcia@gmail.com>
2023-03-30 23:43:16 -0300

    create reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Employee } from './models/employee';
 
 @Component({
@@ -7,11 +8,33 @@ import { Employee } from './models/employee';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public index = -1;
+  public form = new FormGroup({
+    name: new FormControl(''),
+    country: new FormControl(''),
+  });
+
   public employeeList: Employee[] = [
     { id: 1, name: 'Ryan', country: 'USA'},
     { id: 1, name: 'Angelica', country: 'USA'},
     { id: 1, name: 'Joe', country: 'USA'}
-  ]
+  ];
 
+  onSubmit(): void {
+
+  }
+
+  add(e: Employee): void {
+    
+  }
+
+  idit(e: Employee): void{
   
+  }
+
+  delete(e: Employee): void {
+
+  }
+
 }
